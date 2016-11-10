e47899fbdf750018d8b6e5cd3d357f03c5eac64c
Weihao1994 <wcheng3@stevens.edu>
2016-11-10 17:00:59 -0500

    Update CppCode.java

--- code/CppCode.java
+++ code/CppCode.java
@@ -48,18 +48,17 @@ class CppCode extends Code {
 	}
 	public void update() throws IOException {
 		FileWriter fwc = new FileWriter("comment.txt");
-		BufferedWriter bufwc = new BufferedWriter(fw);
+		BufferedWriter bufwc = new BufferedWriter(fwc);
 		bufwc.write(comment);
-		bufwc.newLine();
 		bufwc.flush();
 		bufwc.close();
 		FileWriter fwg = new FileWriter("grade.txt");
-		BufferedWriter bufwg = new BufferedWriter(fw);
-		bufwg.write(grade);
-		bufwg.newLine();
+		BufferedWriter bufwg = new BufferedWriter(fwg);
+		bufwg.write(score + "");
 		bufwg.flush();
 		bufwg.close();
 		// update the comments file and grade file.
 		// the graph group use the grade file to read and graph it  ** Interface 2
 	}
+
 }
