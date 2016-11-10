a15a9c1244cae01678f39457ebd1b4bac736577a
Tao <tjiang6@stevens.edu>
2016-11-10 14:42:55 -0500

    Add files via upload

--- code/Code.java
+++ code/Code.java
@@ -5,27 +5,11 @@ abstract class Code {
 	private String comment;
 
 	Code() {}
-	public double getScore() {
-		return this.score;
-	}
-	public void setScore(double scr) {
-		this.score = scr;
-	}
-	public String getSourceCode() {
-		return this.sourceCode;
-	}
-	public void setSourceCode(String sourceCode) {
-		this.sourceCode = sourceCode;
-	}
 	public void compile() {         // compile the code using cmd
 	}
 	public void display() {                     //** Interface 3 to plagiarism team.
 		//String str = this.getSourceCode();
 		// display the source code.
-	}
-	public double score(double score) {
-		this.score = score;
-		// allow the grader to grade the code based on the result. 
 	}
 	public void setComments() {
 		// allow the grader to comment the code and write the comment into a file. 
